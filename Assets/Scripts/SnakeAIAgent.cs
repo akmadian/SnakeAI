937db0bb01f1bdf4db7b867ea30c2996b28d4f84
Ari Madian <akmadian@gmail.com>
2018-04-19 15:12:04 -0700

    See notes
     - Removed Modified ML-Agents Source dir.
     - SnakeAIAgent
        - Added lines for VectorObs for food and body raycasts.
        - Added VectorObs for score.
        - Added a couple more output lines.
        - Moved InvokeRepeating call from Start() to InitializeAgent().
        - Removed commenting from Move() and adapted it to new file.
        - Added new layerMasks.
        - Reorganized variables.
     -  SnakeAI.py - Not tested
        - Added base for tensorflow code.
        - Added base Tensorflow variables.
        - Added comments to divide tf vars and ML-Agents vars.

--- Assets/Scripts/SnakeAIAgent.cs
+++ Assets/Scripts/SnakeAIAgent.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections;
-using System;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,27 +12,27 @@ public class SnakeAIAgent : Agent {
 	public GameObject head;
 	public GameObject tailPrefab;
 	public float moveSpeed = 0.2f;
+	private float score = 0.0f;
+	int faceAngle = 90;
+	bool ate = false;
 
 	List<Transform> tail = new List<Transform>();
 	Vector3 snakeResetPos = new Vector3(0, 0, 0);
 	Vector2 dir = Vector2.right;
-	int faceAngle = 90;
-	bool ate = false;
 
-	// Layermask to exclude all but walls
-	int layerMask_onlyWalls = 1 << 9;
-	string logFilePath = @"C:\Users\Ai\Desktop\SnakeAI\log.txt";
+	int layerMask_onlyWalls = 1 << 9; // Exclude all but walls
+	int layerMask_onlyFood = 1 << 10;
+	int layerMask_onlyBody = 1 << 11;
 
+	string logFilePath = @"C:\Users\Ari\Desktop\SnakeAI\log.txt";
 
-	/*
-	void Start () {
-		InvokeRepeating("Move", moveSpeed, moveSpeed); 
-	}
-	*/
 
 	public override void InitializeAgent(){
 		base.InitializeAgent();
 		InitializeLogFile();
+		Output("Initialized File");
+		Output("Tail - " + tail.ToString());
+		InvokeRepeating ("Move", 0.2f, moveSpeed);
 	}
 
 	public override void CollectObservations(){
@@ -42,6 +43,22 @@ public class SnakeAIAgent : Agent {
 		AddVectorObs (Physics2D.Raycast (transform.position, dir, Mathf.Infinity, layerMask_onlyWalls).distance); // Forward
 		AddVectorObs (Physics2D.Raycast (transform.position, RotateDeg(dir, 45f), Mathf.Infinity, layerMask_onlyWalls).distance); // Forward Right
 		AddVectorObs (Physics2D.Raycast (transform.position, RotateDeg(dir, -45f), Mathf.Infinity, layerMask_onlyWalls).distance); // Forward Left
+		// Distance to food from head
+		AddVectorObs (Physics2D.Raycast (transform.position, RotateDeg(dir, 90f), Mathf.Infinity, layerMask_onlyFood).distance); // Right
+		AddVectorObs (Physics2D.Raycast (transform.position, RotateDeg(dir, -90f), Mathf.Infinity, layerMask_onlyFood).distance); // Left
+		AddVectorObs (Physics2D.Raycast (transform.position, dir, Mathf.Infinity, layerMask_onlyFood).distance); // Forward
+		AddVectorObs (Physics2D.Raycast (transform.position, RotateDeg(dir, 45f), Mathf.Infinity, layerMask_onlyFood).distance); // Forward Right
+		AddVectorObs (Physics2D.Raycast (transform.position, RotateDeg(dir, 20f), Mathf.Infinity, layerMask_onlyFood).distance); // Forward Right Semi 
+		AddVectorObs (Physics2D.Raycast (transform.position, RotateDeg(dir, -20f), Mathf.Infinity, layerMask_onlyFood).distance); // Forward Left Semi
+		AddVectorObs (Physics2D.Raycast (transform.position, RotateDeg(dir, -45f), Mathf.Infinity, layerMask_onlyFood).distance); // Forward Left
+		// Distance to body from head
+		AddVectorObs (Physics2D.Raycast (transform.position, RotateDeg(dir, 90f), Mathf.Infinity, layerMask_onlyBody).distance); // Right
+		AddVectorObs (Physics2D.Raycast (transform.position, RotateDeg(dir, -90f), Mathf.Infinity, layerMask_onlyBody).distance); // Left
+		AddVectorObs (Physics2D.Raycast (transform.position, dir, Mathf.Infinity, layerMask_onlyBody).distance); // Forward
+		AddVectorObs (Physics2D.Raycast (transform.position, RotateDeg(dir, 45f), Mathf.Infinity, layerMask_onlyBody).distance); // Forward Right
+		AddVectorObs (Physics2D.Raycast (transform.position, RotateDeg(dir, -45f), Mathf.Infinity, layerMask_onlyBody).distance); // Forward Left
+		// Current score
+		AddVectorObs (score);
 		Output("Raycasts found and added");		
 	}
 
@@ -63,11 +80,12 @@ public class SnakeAIAgent : Agent {
 	}
 
 	void InitializeLogFile(){
-		System.IO.File.WriteAllText(@"C:\Users\Ari\Desktop\SnakeAI\log.txt", DateTime.Now.ToString());
+		System.IO.File.WriteAllText(logFilePath, System.DateTime.Now.ToString());
+		using (System.IO.StreamWriter file = new System.IO.StreamWriter (logFilePath, true)) {file.WriteLine("   ")}
 	}
 
 	void Output(string message){
-		using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Ari\Desktop\SnakeAI\log.txt", true)){
+		using (System.IO.StreamWriter file = new System.IO.StreamWriter(logFilePath, true)){
             file.WriteLine("DEBUG LOG - " + message);
         }
 	}
@@ -80,7 +98,7 @@ public class SnakeAIAgent : Agent {
         float ty = baseVector.y;
         return new Vector2(cos * tx - sin * ty, sin * tx + cos * ty);
     }
-    /*
+
     void Move() {
 		// Save current position (gap will be here)
 		Vector2 v = transform.position;
@@ -91,9 +109,8 @@ public class SnakeAIAgent : Agent {
 		// Ate something? Then insert new Element into gap
 		if (ate) {
 			// Load Prefab into the world
-			GameObject g = (GameObject)Instantiate (tailPrefab,
-					        v,
-					        Quaternion.identity);
+			GameObject g = (GameObject)Instantiate (tailPrefab, v, Quaternion.identity);
+			score += 1.0f;
 
 			// Keep track of it in our tail list
 			tail.Insert (0, g.transform);
@@ -102,14 +119,14 @@ public class SnakeAIAgent : Agent {
 			ate = false;
 		} else if (tail.Count > 0) {	// Do we have a Tail?
 				// Move last Tail Element to where the Head was
-				tail.Last ().position = v;
+			tail.Last().position = v;
 
 				// Add to front of list, remove from the back
 				tail.Insert (0, tail.Last ());
 				tail.RemoveAt (tail.Count - 1);
 		}
 	}
-	*/
+	
 
 	public void GenNewDir(string dirtogo){
 		if (dir == Vector2.right){
--- SnakeAI.py
+++ SnakeAI.py
@@ -17,7 +17,7 @@ import arrow
 print(arrow.utcnow().to('US/Pacific'))
 print('Imported')
 
-
+# ML-Agents Variables
 env_name = "SnakeAI"
 train_mode = True
 env = UnityEnvironment(file_name=env_name)
@@ -25,19 +25,65 @@ default_brain = env.brain_names[0]
 brain = env.brains[default_brain]
 brain_info = None
 
+# Tensorflow Variables
+n_nodes_hl1 = 15
+n_nodes_hl2 = 20
+n_nodes_hl3 = 15
+n_actions = 4
+n_epochs = 10
 
+x = tf.placeholder('float', [None, 17])
+y = tf.placeholder('float')
 
 env.reset()
 print('env.reset()')
-binfo = env.step(1)
-print(type(binfo))
-b_ = binfo['Brain']
-print(b_.vector_observations)
-for key, value in binfo.items():
-	print(str(key) + ' - ' + str(value))
-	print(vars(value))
-print('env.step()')
-print('__________')
+binfo = env.step(1)['Brain']
+vector_observations = binfo.vector_observations[0][:-1]
+train_neural_network(vector_observations)
+
+def neural_network_model(data):
+	hidden_l1 = {'weights': tf.Variable(tf.random_normal([17, n_nodes_hl1])),
+				 'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
+	hidden_l2 = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+				 'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
+	hidden_l3 = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+				 'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
+
+	output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_actions])),
+				    'biases': tf.Variable(tf.random_normal(n_actions))}
+
+	l1 = tf.add(tf.matmul(data, hidden_l1['weights']), hidden_l1['biases'])
+	l1 = tf.nn.relu(l1)
+
+	l2 = tf.add(tf.matmul(l1, hidden_l2['weights']), hidden_l2['biases'])
+	l2 = tf.nn.relu(l2)
+
+	l3 = tf.add(tf.matmul(l2, hidden_l3['weights']), hidden_l3['biases'])
+	l3 = tf.nn.relu(l3)
+
+	output = tf.add(tf.matmul(l3, output_layer['weights']) + output_layer['biases'])
+
+	return output
+
+def train_neural_network(x):
+	prediction = neural_network_model(x)
+	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y))
+	optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+	with tf.Session() as sess:
+		sess.run(tf.initialize_all_variables())
+
+		for epoch in range(n_epochs):
+			e_loss = 0
+			for _ in range(1):
+				e_x, e_y = None
+				_, c = sess.run([optimizer, cost], feed_dict = {x: e_x, y: e_y})
+				e_loss += c
+			print('Epoch #' + str(epoch) + ' of ' + str(n_epochs) + ' complete - Loss: ' + str(e_loss))
+
+		correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+		accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+		print('Accuracy: ' + accuracy.eval({x: 'mnist.test.images', y:'mnist.test.labels'}))
 
 
 env.reset(train_mode=train_mode, config=None)
